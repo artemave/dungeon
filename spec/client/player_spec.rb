940d4cf351e3d1ea300a6e06a7f2d4c7a451ffaa
artemave <artemave@gmail.com>
2011-05-20 23:34:51 +0100

    starting player spec

---
+++ spec/client/player_spec.rb
@@ -0,0 +1,11 @@
+require 'client/player'
+
+describe Player do
+  context 'in order to start the game' do
+    it 'should enter the dungeon'
+  end
+
+  context 'in order to win the game' do
+    it 'should find the treasure chamber'
+  end
+end
