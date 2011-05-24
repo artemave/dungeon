8c10ac960f9bc6fccf256103ba3b489b30580c5e
artem <artemave@gmail.com>
2011-05-24 17:53:23 +0200

    start map speced

--- spec/client/dungeon_map_spec.rb
+++ spec/client/dungeon_map_spec.rb
@@ -2,8 +2,22 @@ require './spec/spec_helper'
 require 'client/dungeon_map'
 
 describe DungeonMap do
+  before do
+    @map = DungeonMap.new
+  end
+
   context 'in order for player to be able to track his path' do
-    it 'should allow to start map with entrance'
+    it 'should allow to start map with entrance' do
+      entrance = double('Room', :id => 1)
+
+      @map.start(entrance)
+
+      tree = @map.instance_variable_get(:@tree)
+      tree.should be_is_root
+      tree.name.should be entrance.id
+      tree.content.should be entrance
+    end
+
     it 'should allow to put room on the map'
     it 'should allow to lookup room'
     it 'should allow to lookup entrance to particular room'
