863a75355c437ee940a586579f8ce9d41a4f7804
artem <artemave@gmail.com>
2011-05-24 12:35:14 +0200

    integration spec added

---
+++ spec/integration/game_spec.rb
@@ -0,0 +1,14 @@
+require './spec/spec_helper'
+
+describe 'dungeon game' do
+  it 'is played in dungeon'
+  it 'is played by player'
+
+  it 'starts with player entering dungeon'
+
+  it 'consists of player visiting room according to chosen strategy'
+
+  it 'is won when player found treasure chamber'
+
+  it 'is lost if player has not found treasure chamber'
+end
