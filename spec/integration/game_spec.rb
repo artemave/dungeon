854052b5fbaa9803108f9d6398409be6cdd86cf0
artemave <artemave@gmail.com>
2011-05-26 22:30:54 +0100

    wip

--- spec/integration/game_spec.rb
+++ spec/integration/game_spec.rb
@@ -1,14 +1,29 @@
 require './spec/spec_helper'
 
-describe 'dungeon game' do
-  it 'is played in dungeon'
-  it 'is played by player'
+describe 'adventure game' do
+  it 'starts with player entering dungeon' do
+    player = Player.new
+    dungeon = Dungeon.new
+    player.enter(dungeon)
 
-  it 'starts with player entering dungeon'
+    player.dungeon.should be dungeon
+    player.current_room.should be dungeon.entrance
+  end
 
-  it 'consists of player visiting room according to chosen strategy'
+  it 'consists of player visiting room according to chosen strategy' do
+    player = Player.new
+    dungeon = Dungeon.new
+    player.enter(dungeon)
 
-  it 'is won when player found treasure chamber'
+    player.strategy = GameStrategy::NoBrainer.new
+
+    player.next_room.should be room1
+    player.next_room.should be room2
+    player.next_room.should be_in [room3, room4]
+  end
+
+  it 'is won when player found treasure chamber' do
+  end
 
   it 'is lost if player has not found treasure chamber'
 end
